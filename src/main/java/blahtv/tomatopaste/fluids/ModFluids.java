74231eaa70d81a5d379c4d86f577943e6724bf1e
ecg5b6 <ecg5b6@umsystem.edu>
2023-04-04 06:23:15 -0500

    first commit

---
+++ src/main/java/blahtv/tomatopaste/fluids/ModFluids.java
@@ -0,0 +1,12 @@
+package blahtv.tomatopaste.fluids;
+
+import net.minecraft.world.level.material.Fluids;
+
+public class ModFluids {
+    class TomatoSauce extends Fluids{
+        TomatoSauce()
+        {
+            //FluidRegistry.
+        }
+    }
+}
